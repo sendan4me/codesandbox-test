c9bf0761abc788e3b0e2630f378a9af53da3013b
sendan4me <59216921+sendan4me@users.noreply.github.com>
2022-04-06 15:38:09 +0900

    test-2

--- src/index.js
+++ src/index.js
@@ -9,3 +9,27 @@ document.getElementById("app").innerHTML = `
 </div>
 <p>hello my world by Kenny</p>
 `;
+//分割代入
+const myProfile = {
+  name: "kenny",
+  age: 38
+};
+
+const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+console.log(message1);
+
+const { name, age } = myProfile;
+const message2 = `名前は${name}です。年齢は${age}歳です。`;
+console.log(message2);
+
+const myProfile1 = ["じゃけぇ", 28];
+
+const message3 = `名前は${myProfile1[0]}です。年齢は${myProfile1[1]}歳です。`;
+console.log(message3);
+
+const message4 = `名前は${name}です。年齢は${age}歳です。`;
+console.log(message4);
+
+//デフォルト値、引数など
+const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん!`);
+sayHello();
